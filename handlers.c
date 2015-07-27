e30f5d095d15dab68ecfdfe4160a1aadcf432051
Gregor Best <gbe@unobtanium.de>
2015-07-27 14:12:20 +0200

    Add first draft of rtadv listener
    Ugly code, but what can ya do?

--- handlers.c
+++ handlers.c
@@ -1,10 +1,16 @@
 #include <err.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include <sys/types.h>
+#include <sys/time.h>
+#include <sys/ioctl.h>
+#include <net/bpf.h>
+#include <sys/socket.h>
+#include <net/if.h>
 #include <sys/queue.h>
 #include <sys/uio.h>
 #include <imsg.h>
@@ -69,4 +75,82 @@ dhcpv4_handle_update(int fd, int msg_fd) {
 		}
 	} while (errno == EAGAIN);
 	err(1, "msgbuf_write");
+}
+
+#define BPF_FORMAT "/dev/bpf%d"
+
+int
+rtadv_open_bpf(const char *dev) {
+	char filename[50];
+	struct ifreq ifr;
+	int b, sock;
+
+	for (b = 0; ; b++) {
+		snprintf(filename, sizeof(filename), BPF_FORMAT, b);
+		sock = open(filename, O_RDWR | O_CLOEXEC, 0);
+		if (sock >= 0) {
+			break;
+		}
+		if (errno == EBUSY) {
+			continue;
+		}
+		err(1, "Can't find a free BPF");
+	}
+
+	(void)strlcpy(ifr.ifr_name, dev, IFNAMSIZ);
+	if (ioctl(sock, BIOCSETIF, &ifr) < 0)
+		err(1, "Can't attach interface %s to BPF %s", dev, filename);
+
+	return sock;
+}
+
+/* Generated with tcpdump -dd 'icmp6 and ip6[40] == 134' */
+struct bpf_insn rtadv_bpf_filter[] = {
+	{ 0x28, 0, 0, 0x0000000c },
+	{ 0x15, 0, 5, 0x000086dd },
+	{ 0x30, 0, 0, 0x00000014 },
+	{ 0x15, 0, 3, 0x0000003a },
+	{ 0x30, 0, 0, 0x00000036 },
+	{ 0x15, 0, 1, 0x00000086 },
+	{ 0x6, 0, 0, 0x00000074 },
+	{ 0x6, 0, 0, 0x00000000 },
+};
+
+int
+rtadv_setup_bpf(const char *dev) {
+	struct bpf_version v;
+	struct bpf_program p;
+	int sock, flag = 1;
+
+	sock = rtadv_open_bpf(dev);
+
+	if (ioctl(sock, BIOCVERSION, &v) < 0)
+		err(1, "can't get BPF version");
+
+	if (v.bv_major != BPF_MAJOR_VERSION ||
+	    v.bv_minor != BPF_MINOR_VERSION) {
+		err(1, "BPF version mismatch");
+	}
+
+	if (ioctl(sock, BIOCIMMEDIATE, &flag) < 0) {
+		err(1, "can't set immediate flag on BPF socket");
+	}
+
+	if (ioctl(sock, BIOCSFILDROP, &flag) < 0) {
+		err(1, "can't set filter drop flag on BPF socket");
+	}
+
+	p.bf_len = sizeof(rtadv_bpf_filter) / sizeof(rtadv_bpf_filter[0]);
+	p.bf_insns = rtadv_bpf_filter;
+
+	if (ioctl(sock, BIOCSETF, &p) < 0) {
+		err(1, "can't attach filter to BPF socket");
+	}
+
+	return sock;
+}
+
+void
+rtadv_handle_update(int fd, int msgfd) {
+	fprintf(stderr, "packet available on FD %d\n", fd);
 }
--- handlers.h
+++ handlers.h
@@ -1,1 +1,4 @@
 void dhcpv4_handle_update(int, int);
+
+int rtadv_setup_bpf(char*);
+void rtadv_handle_update(int, int);
